4d1375386931b0927c9eadab4ad883159e520d85
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2021-09-11 13:39:58 +0700

    Create mendelsFirstLaw.java

---
+++ mendelsFirstLaw.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+/******************************\
+ * The solution is at the top *
+ *                            *
+ *   Created by : azhar556    *
+\******************************/
+
+public class A {
+	static long seluncur (int x) {
+		return x * (x - 1) / 2;
+	}
+    static void solve() {
+		int k = ni();
+		int m = ni();
+		int n = ni();
+		long totDominant = (seluncur(k) * 4) + ((long)k * m * 4) + ((long)k * n * 4) + (seluncur(m) * 3) + ((long)m * n * 2);
+		// err.println(totDominant);
+		out.printf("%.5f", 1.0 * totDominant / (seluncur(k + m + n) * 4));
+    }    
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        int t;// = ni();
+        t = 1;
+        while (t-- > 0) solve(); 
+        err.println("Time elapsed : " + (System.currentTimeMillis() - time) / 1000F + " s.");
+        err.close(); out.close();
+    }
+
+	static int[] radixSort(int[] f){ return radixSort(f, f.length); }
+	static int[] radixSort(int[] f, int n)
+	{
+		int[] to = new int[n];
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(f[i]&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[f[i]&0xffff]++] = f[i];
+			int[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(f[i]>>>16)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[f[i]>>>16]++] = f[i];
+			int[] d = f; f = to;to = d;
+		}
+		return f;
+	}
+
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static PrintWriter err = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.err)));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer token;
+        
+    static String ns() {
+        while (token == null || !token.hasMoreTokens()) {
+            try {
+                token = new StringTokenizer(br.readLine());
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return token.nextToken();
+    }
+    static char nc() {
+        return Character.valueOf(ns().charAt(0));
+    }
+    static int ni() {
+        return Integer.parseInt(ns());
+    }
+    static double nd() {
+        return Double.parseDouble(ns());
+    }
+    static long nl() {
+        return Long.parseLong(ns());
+    }
+}
+// Collections Arrays Math
+// Vector HashSet TreeSet HashMap TreeMap ArrayDeque
