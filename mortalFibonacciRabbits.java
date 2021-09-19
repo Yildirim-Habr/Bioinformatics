8ebda272259eee59cf9a410fcf176ca1ed6a2706
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2021-09-19 09:20:37 +0700

    Create mortalFibonacciRabbits.java

---
+++ mortalFibonacciRabbits.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+/******************************\
+ * The solution is at the top *
+ *                            *
+ *   Created by : azhar556    *
+\******************************/
+
+public class A {
+    static void solve() {
+		int n = ni();
+		int m = ni();
+		long[][] dp = new long[2][n];
+		dp[0][0] = 1;
+		dp[1][0] = 0;
+		for (int i = 1; i < n; i++) {
+			dp[0][i] = dp[1][i - 1];
+			dp[1][i] = dp[0][i - 1] + dp[1][i - 1];
+			if (i >= m) {
+				dp[1][i] -= dp[0][i - m];
+			}
+		}
+		out.println(dp[0][n - 1] + dp[1][n - 1]);
+    }    
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        int t;// = ni();
+        t = 1;
+        while (t-- > 0) solve(); 
+        err.println("\nTime elapsed : " + (System.currentTimeMillis() - time) / 1000F + " s.\n");
+        err.close(); out.close();
+    }
+
+    static int[] radixSort(int[] f){ return radixSort(f, f.length); }
+    static int[] radixSort(int[] f, int n)
+    {
+        int[] to = new int[n];
+        {
+            int[] b = new int[65537];
+            for(int i = 0;i < n;i++)b[1+(f[i]&0xffff)]++;
+            for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+            for(int i = 0;i < n;i++)to[b[f[i]&0xffff]++] = f[i];
+            int[] d = f; f = to;to = d;
+        }
+        {
+            int[] b = new int[65537];
+            for(int i = 0;i < n;i++)b[1+(f[i]>>>16)]++;
+            for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+            for(int i = 0;i < n;i++)to[b[f[i]>>>16]++] = f[i];
+            int[] d = f; f = to;to = d;
+        }
+        return f;
+    }
+
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static PrintWriter err = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.err)));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer token;
+        
+    static String ns() {
+        while (token == null || !token.hasMoreTokens()) {
+            try {
+                token = new StringTokenizer(br.readLine());
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return token.nextToken();
+    }
+    static char nc() {
+        return Character.valueOf(ns().charAt(0));
+    }
+    static int ni() {
+        return Integer.parseInt(ns());
+    }
+    static double nd() {
+        return Double.parseDouble(ns());
+    }
+    static long nl() {
+        return Long.parseLong(ns());
+    }
+}
+// Collections Arrays Math
+// Vector HashSet TreeSet HashMap TreeMap ArrayDeque
